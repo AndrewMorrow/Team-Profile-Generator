9063d10b32dc9a78b102525c09da4fb0c8193eb2
AndrewMorrow <beardedmongo@gmail.com>
2020-11-17 00:54:31 -0600

    adds render function call

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
+const employeeArray = [];
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
@@ -60,7 +61,9 @@ function askMember() {
                 }
             });
         } else {
-            console.log("stop");
+            // console.log("stop");
+
+            writeHtml("index.html", render(employeeArray));
         }
     });
 }
@@ -97,6 +100,9 @@ function intern() {
             res.internEmail,
             res.internSchool
         );
+        employeeArray.push(person);
+        // console.log(person);
+        askMember();
     });
 }
 function manager() {
@@ -152,6 +158,10 @@ function engineer() {
     inquirer.prompt(questions).then((res) => {
         console.log(res);
     });
+}
+
+function writeHtml(filename, res) {
+    return fs.writeFileSync(path.join(__dirname, filename), res);
 }
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
